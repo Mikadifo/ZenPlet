e51f4627de0c07e50114f66e823e540a16b79aa4
Michael <michaelpadillaheredia@hotmail.com>
2021-03-09 18:42:57 -0500

    Created botton nav bar

---
+++ app/src/main/java/com/mikadifo/zenplet/nav/BottomNavActivity.java
@@ -0,0 +1,31 @@
+package com.mikadifo.zenplet.nav;
+
+import com.mikadifo.zenplet.R;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class BottomNavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_nav);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_lost_pets, R.id.navigation_health, R.id.navigation_pets)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+
+}
