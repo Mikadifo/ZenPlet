467bc6e0b69ffdb0ee1150c2f173cf47fbb59f28
Dormamud03 <paulsanunga@gmail.com>
2021-03-10 17:23:38 -0500

    Implementacion del boceto vacunas editar, lista, nuevo

---
+++ app/src/main/java/com/mikadifo/zenplet/ui/vaccines/EditVaccines.java
@@ -0,0 +1,66 @@
+package com.mikadifo.zenplet.ui.vaccines;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mikadifo.zenplet.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link EditVaccines#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EditVaccines extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public EditVaccines() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment EditVaccines.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static EditVaccines newInstance(String param1, String param2) {
+        EditVaccines fragment = new EditVaccines();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_vaccines, container, false);
+    }
+}
