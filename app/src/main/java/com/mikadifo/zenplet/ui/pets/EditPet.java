52fde5ea7eaab5c89e93935788f77d642f7957ee
Fernanda Toledo <maria.toledo.est@tecazuay.edu.ec>
2021-03-11 16:45:50 -0500

    metodos de pet and edit pet cambiar nombres Camara y conectar los fragments .....

--- app/src/main/java/com/mikadifo/zenplet/ui/pets/EditPet.java
+++ app/src/main/java/com/mikadifo/zenplet/ui/pets/EditPet.java
@@ -1,12 +1,21 @@
 package com.mikadifo.zenplet.ui.pets;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.mikadifo.zenplet.R;
 
@@ -25,6 +34,8 @@ public class EditPet extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    ImageView imageView;
+    private View root;
 
     public EditPet() {
         // Required empty public constructor
@@ -55,6 +66,30 @@ public class EditPet extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        Button btn = root.findViewById(R.id.btnCreatPet);
+
+         btn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager
+        .beginTransaction()
+        .replace(R.id.nav_host_fragment, new FragmentPets());
+        fragmentTransaction.commit();
+        }
+        });
+        Button btno = root.findViewById(R.id.btnLost);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager
+                        .beginTransaction()
+                        .replace(R.id.nav_host_fragment, new PostLostPet());
+                fragmentTransaction.commit();
+            }
+        });
     }
 
     @Override
@@ -63,4 +98,42 @@ public class EditPet extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_edit_pet, container, false);
     }
+    public void accesoCamara(View view){
+        //llamar a un recurso desde el intent - recurso para la camara
+        Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getActivity().getPackageManager())!=null){
+            //manejar el resultado
+            startActivityForResult(intent,1);
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //comprobar si hay respuesta y resultado
+        if (requestCode==1&& resultCode==1){
+            //Recibir imagen
+            Bundle bundle=data.getExtras();
+            Bitmap image= (Bitmap)bundle.get("data");
+            //mostrar imagen en la pantalla
+            imageView.setImageBitmap(image);
+        }
+        if (resultCode==1){
+            Uri path=data.getData();
+            imageView.setImageURI(path);
+        }
+
+    }
+    public void onclick(View view) {
+        cargarImagen();
+    }
+    public void cargarImagen(){
+        //Acceder al contenido de la galeria
+        Intent intent= new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        //recibir el resultado de la foto
+        startActivityForResult(Intent.createChooser(intent,"Seleccione la aplicacion"), 10);
+    }
+
 }
