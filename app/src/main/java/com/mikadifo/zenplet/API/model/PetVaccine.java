9671e7fda57a28052867f2bfdd644ecb1070bc63
Fernanda Toledo <maria.toledo.est@tecazuay.edu.ec>
2021-03-24 18:00:11 -0500

    servicios y modelos de LostPet y PetVaccine

--- app/src/main/java/com/mikadifo/zenplet/API/model/PetVaccine.java
+++ app/src/main/java/com/mikadifo/zenplet/API/model/PetVaccine.java
@@ -1,4 +1,54 @@
 package com.mikadifo.zenplet.API.model;
 
 public class PetVaccine {
+
+    private Long PetVaccinesId ;
+
+    private String petVaccineDate;
+
+    private String petVaccineNext;
+
+    public Long getPetVaccinesId() {
+        return PetVaccinesId;
+    }
+
+    public void setPetVaccinesId(Long petVaccinesId) {
+        PetVaccinesId = petVaccinesId;
+    }
+
+    public String getPetVaccineDate() {
+        return petVaccineDate;
+    }
+
+    public void setPetVaccineDate(String petVaccineDate) {
+        this.petVaccineDate = petVaccineDate;
+    }
+
+    public String getPetVaccineNext() {
+        return petVaccineNext;
+    }
+
+    public void setPetVaccineNext(String petVaccineNext) {
+        this.petVaccineNext = petVaccineNext;
+    }
+
+    public PetVaccine(String petVaccineDate) {
+        this.petVaccineDate = petVaccineDate;
+    }
+
+    public PetVaccine(Long petVaccinesId, String petVaccineDate, String petVaccineNext) {
+        PetVaccinesId = petVaccinesId;
+        this.petVaccineDate = petVaccineDate;
+        this.petVaccineNext = petVaccineNext;
+
+    }
+
+    @Override
+    public String toString() {
+        return "PetVaccine{" +
+                "PetVaccinesId=" + PetVaccinesId +
+                ", petVaccineDate='" + petVaccineDate + '\'' +
+                ", petVaccineNext='" + petVaccineNext + '\'' +
+                '}';
+    }
 }
--- app/src/main/java/com/mikadifo/zenplet/API/service/LostPetService.java
+++ app/src/main/java/com/mikadifo/zenplet/API/service/LostPetService.java
@@ -1,4 +1,30 @@
 package com.mikadifo.zenplet.API.service;
 
+import com.mikadifo.zenplet.API.model.LostPet;
+import com.mikadifo.zenplet.API.model.Pet;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Query;
+
 public interface LostPetService {
+
+    @GET("/api/lost-pet")
+    Call<List<Pet>> getLostPets();
+
+    @POST("/api/save-lost-pet")
+    Call<LostPet> saveLostPet(@Body LostPet lostpet);
+
+    @PUT("/api/edit-lost-pet")
+    Call<LostPet> updateLostPet(@Query("id") long id, @Body LostPet lostpet);
+
+    @DELETE("/api/delete-lost-pet")
+    Call deleteLostPet(@Query("id") long id);
+
 }
