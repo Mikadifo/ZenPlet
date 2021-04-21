a4597493c2e5021963d86f11e16ef55f0b5d6c6a
Fernanda Toledo <maria.toledo.est@tecazuay.edu.ec>
2021-04-21 14:50:39 -0500

    Arreglar mensajes de sigin up y login

--- app/src/main/java/com/mikadifo/zenplet/API/CallWithToken.java
+++ app/src/main/java/com/mikadifo/zenplet/API/CallWithToken.java
@@ -12,9 +12,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class CallWithToken {
     public static String token = "";
 //    public static final String BASE_URL = "https://zenplet.herokuapp.com/";
-   // public static final String BASE_URL = "http://192.168.2.102:8080/"; //mika
+   public static final String BASE_URL = "http://192.168.2.102:8080/"; //mika
 //    public static final String BASE_URL = "http://192.168.1.100:8080/"; //tito
-      public static final String BASE_URL = "http://192.168.1.101:8080/"; //mika
+     // public static final String BASE_URL = "http://192.168.1.101:8080/"; //mika
 
     public Retrofit getCallToken() {
         OkHttpClient okHttpClient = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
